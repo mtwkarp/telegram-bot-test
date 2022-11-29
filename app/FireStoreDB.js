e2814984c424d39ac23a9d6ab321ec93845ea536
Matvii Karpenko <42414976+mtwkarp@users.noreply.github.com>
2022-11-29 22:16:14 +0200

    added heroku deploy action

--- app/FireStoreDB.js
+++ app/FireStoreDB.js
@@ -51,18 +51,8 @@ class FireStoreDB {
 
   async initApp() {
     const privateKey = process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replaceAll('\\n','\n');
-    console.log({
-      "type": process.env.SERVICE_ACCOUNT_TYPE,
-      "project_id": process.env.SERVICE_ACCOUNT_PROJECT_ID,
-      "private_key_id": process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-      "private_key": privateKey,
-      "client_email": process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-      "client_id": process.env.SERVICE_ACCOUNT_CLIENT_ID,
-      "auth_uri": process.env.SERVICE_ACCOUNT_AUTH_URI,
-      "token_uri": process.env.SERVICE_ACCOUNT_TOKEN_URI,
-      "auth_provider_x509_cert_url": process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
-      "client_x509_cert_url": process.env.SERVICE_ACCOUNT_CLIENT_CERT_URL
-    })
+
+    console.log('heroku test action')
     await initializeApp({
       credential: cert({
         "type": process.env.SERVICE_ACCOUNT_TYPE,
