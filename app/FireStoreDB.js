0cd2cbcde95dfb484939cf80c9a1cd728f6337cf
Matvii Karpenko <42414976+mtwkarp@users.noreply.github.com>
2022-11-29 19:03:36 +0200

    updated firebase file

--- app/FireStoreDB.js
+++ app/FireStoreDB.js
@@ -1,6 +1,5 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore} = require('firebase-admin/firestore');
-const serviceAccount = require('./service-account.json');
 
 class FireStoreDB {
   constructor() {
