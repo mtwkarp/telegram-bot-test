0a079d9c2cc621dc13887084699166d29e8d80fc
Matvii Karpenko <42414976+mtwkarp@users.noreply.github.com>
2022-11-29 19:08:41 +0200

    updated firebase file

--- app/FireStoreDB.js
+++ app/FireStoreDB.js
@@ -50,7 +50,18 @@ class FireStoreDB {
   }
 
   async initApp() {
-    console.log(process.env.SERVICE_ACCOUNT_PRIVATE_KEY)
+    console.log({
+      "type": process.env.SERVICE_ACCOUNT_TYPE,
+      "project_id": process.env.SERVICE_ACCOUNT_PROJECT_ID,
+      "private_key_id": process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+      "private_key": process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+      "client_email": process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+      "client_id": process.env.SERVICE_ACCOUNT_CLIENT_ID,
+      "auth_uri": process.env.SERVICE_ACCOUNT_AUTH_URI,
+      "token_uri": process.env.SERVICE_ACCOUNT_TOKEN_URI,
+      "auth_provider_x509_cert_url": process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
+      "client_x509_cert_url": process.env.SERVICE_ACCOUNT_CLIENT_CERT_URL
+    })
     await initializeApp({
       credential: cert({
         "type": process.env.SERVICE_ACCOUNT_TYPE,
