29c39fb91e920a02d3b72deaa7cf73664a2fc020
Matvii Karpenko <42414976+mtwkarp@users.noreply.github.com>
2023-02-13 16:45:00 +0200

    save

--- src/googleServices/services/DriveService.ts
+++ src/googleServices/services/DriveService.ts
@@ -1,7 +1,6 @@
-import { type GoogleServiceInterface } from '../ts/g_services_interfaces';
 import { type GoogleAuth } from 'google-auth-library';
 import { type JSONClient } from 'google-auth-library/build/src/auth/googleauth';
-export default class DriveService implements GoogleServiceInterface {
+export default class DriveService {
   initService(authenticationObj: GoogleAuth<JSONClient>): void {
   }
 }
