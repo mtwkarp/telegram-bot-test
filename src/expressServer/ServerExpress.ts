2b634cfa58e9b79ca83a54ed8154505f05b82841
Matvii Karpenko <42414976+mtwkarp@users.noreply.github.com>
2023-02-15 12:52:05 +0200

    updated build

--- src/expressServer/ServerExpress.ts
+++ src/expressServer/ServerExpress.ts
@@ -21,7 +21,6 @@ class ServerExpress {
             res.status(200).json({ message: 'It worked!' });
             console.log('hello boy');
         });
-
 // start the server listening for requests
         app.listen(process.env.PORT || 3000,
             () => console.log('Server is running...'));
